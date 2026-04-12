ee2a2e827a9fd888aa53f8d656b240fbe402f463
marcscama <32490781+marcscama@users.noreply.github.com>
2026-04-12 17:58:12 +0200

    Creating User model with MongoDB schema for user

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    preferences: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Preferences'
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
